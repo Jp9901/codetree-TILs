16a2c113ef40546f8aa57f45fdefad2eefe3ca89
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-04 20:12:37 +0900

    Title: 알파벳 출력하기 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240304/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+A = ord("A")
+for i in range(n):
+    for j in range(n):
+        if i > j:
+            print(" ", end = " ")
+        else:
+            print(chr(A), end = " ")
+            A += 1
+
+    print()
