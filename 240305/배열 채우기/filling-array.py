13e90e031c7d5f8032adcea84f86a7ac49230836
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-05 11:56:53 +0900

    Title: 배열 채우기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/배열 채우기/filling-array.py
+++ 240305/배열 채우기/filling-array.py
@@ -1,7 +1,13 @@
-arr = list(map(int,input().split()))
+# 배열에 주어진 수를 입력받아 저장합니다.
+arr = list(map(int, input().split()))
+cnt = 0
 
-if arr[-1] == 0:
-    arr.pop()
+# 배열에 0이 있는지 확인합니다.
+for elem in arr:
+	if elem == 0:
+		break
+	cnt += 1
 
-for ele in arr[::-1]:
-    print(ele, end = " ")
+# 0이 입력되기 전까지의 수를 반대 순서로 출력합니다.
+for i in range(cnt - 1, -1, -1):
+	print(arr[i], end=" ")
