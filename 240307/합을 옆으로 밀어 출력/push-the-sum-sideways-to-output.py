99980e09f74760da1c3e250503fdabe33f77ab96
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-07 21:37:02 +0900

    Title: 합을 옆으로 밀어 출력, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.py
+++ 240307/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.py
@@ -4,5 +4,5 @@ arr = list()
 for _ in range(n):
     arr.append(int(input()))
 
-sum_val = list(str(sum(arr)))
-print(''.join(sum_val[1:]),sum_val[0], sep="")
+sum_val = str(sum(arr))
+print(sum_val[1:],sum_val[0], sep="")
