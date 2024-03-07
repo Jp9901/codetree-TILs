bf75c1363f50db698d90937afbb28f23e1e1f8df
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-07 21:45:19 +0900

    Title: 미는 횟수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240307/미는 횟수/number-of-pushes.py
+++ 240307/미는 횟수/number-of-pushes.py
@@ -6,8 +6,8 @@ n = len(A)
 cnt = 0
 for i in range(n):
     A = A[1:]+A[0]
-    cnt += 1
     if A == B:
+        cnt=i+1
         break
 
-print(-1 if cnt == 0 else cnt)
+print(-1 if cnt==0 else cnt)
