d69f573f1fe5aeb2d5152518f5bc14468669d5ae
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-08 12:08:23 +0900

    Title: 그 계절, 그 날, Time: 150ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240308/그 계절, 그 날/that-season-that-day.py
+++ 240308/그 계절, 그 날/that-season-that-day.py
@@ -1,29 +1,30 @@
 def season(Y,M,D):
-    if M== (1 or 3 or 5 or 7 or 8 or 10 or 12):
+    if M==1 or M==3 or M==5 or M==7 or M==8 or M==10 or M==12:
+
         if D >31:
             return -1
-        if M==3 or 5:
+        if M==3 or M==5:
             return 'Spring'
-        elif M == 7 or 8:
+        elif M == 7 or M==8:
             return "Summer"
         elif M==10:
             return "Fall"
         else:
             return "Winter"
     
-    elif M== (4 or 6 or 9 or 11):
+    elif M==4 or M==6 or M==9 or M==11:
+
         if D >30:
             return -1
         if M==4:
             return 'Spring'
         elif M == 6:
             return "Summer"
-        elif M==9:
-            return "Fall"
         else:
-            return "Winter"
+            return "Fall"
     
     else:
+
         if Y%4 == 0  and Y%100 == 0 and Y%400 ==0:
             if D > 29:
                 return -1
