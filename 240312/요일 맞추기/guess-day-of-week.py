bcc403d122837696d5270fb27fcbf874aafc451b
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-12 11:30:07 +0900

    Title: 요일 맞추기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/요일 맞추기/guess-day-of-week.py
+++ 240312/요일 맞추기/guess-day-of-week.py
@@ -2,9 +2,12 @@ m1, d1, m2, d2 = tuple(map(int,input().split()))
 
 month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-elapsed_d = (month[m2]+d2) - (month[m1]+d1)
+elapsed_d = (month[m1]-d1) + d2
+for mon in range(m1+1,m2):
+    elapsed_d += month[mon]
 
 pos = abs(elapsed_d)%7
+
 if elapsed_d < 0:
     print(day[-pos])
 else:
