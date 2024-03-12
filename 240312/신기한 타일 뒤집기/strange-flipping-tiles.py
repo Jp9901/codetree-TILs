6eece16c4ef014b6f2d57a7be5de0297b16045a9
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-12 13:46:40 +0900

    Title: 신기한 타일 뒤집기, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240312/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240312/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -3,8 +3,12 @@ arr =[
     input().split() for _ in range(n)   
 ]
 
-tile = [0 for _ in range(201)]
-pos = 100
+sum_val = 0
+for i in range(n):
+    sum_val += int(arr[i][0])
+
+tile = [0 for _ in range(2*sum_val+1)]
+pos = sum_val
 
 for i in range(n):
     x = int(arr[i][0])
