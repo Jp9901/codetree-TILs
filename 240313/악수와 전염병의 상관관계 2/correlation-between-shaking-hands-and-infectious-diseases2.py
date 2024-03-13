68bce53680b16899bc31238617c7c97033507b08
Junpyo99 <135187850+Jp9901@users.noreply.github.com>
2024-03-13 11:46:29 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240313/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,19 +1,45 @@
-N,K,P,T = map(int, input().split())
+# 클래스 선언
+class Shake:
+    def __init__(self, time, person1, person2):
+        self.time, self.person1, self.person2 = time, person1, person2
 
-arr = [
-    list(map(int,input().split())) for _ in range(T)
-]
-arr.sort(key = lambda x:x[0])
+# 변수 선언 및 입력
+n, k, p, t = tuple(map(int, input().split()))	
+shakes = []
+for _ in range(t):
+    time, person1, person2 = tuple(map(int, input().split()))
+    shakes.append(Shake(time, person1, person2))
 
-dev = [0 for _ in range(N+1)]
-dev[P] = 1
+shake_num = [0] * (n + 1)
+infected = [False] * (n + 1)
 
-cnt = [0 for _ in range(N+1)]
-for i, x, y in arr:
-    if (dev[x]==1 and cnt[x]<=K) or (dev[y]==1 and cnt[y]<=K):
-        dev[x] = 1
-        cnt[x] += 1
-        dev[y] = 1
-        cnt[y] += 1
+infected[p] = True
 
-print(''.join(map(str,dev[1:])))
+# Custom Comparator를 활용한 정렬
+shakes.sort(key = lambda x: x.time)
+
+# 각 악수 횟수를 세서,
+# K번 초과로 악수를 했을 시 전염시키지 않습니다.
+for shake in shakes:
+	target1 = shake.person1
+	target2 = shake.person2
+	
+	# 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+	if infected[target1]:
+		shake_num[target1] += 1
+	if infected[target2]:
+		shake_num[target2] += 1
+	
+	# target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+	if shake_num[target1] <= k and infected[target1]:
+		infected[target2] = True
+	
+	# target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+	if shake_num[target2] <= k and infected[target2]:
+		infected[target1] = True
+		
+for i in range(1, n + 1):
+	if infected[i]:
+		print(1, end="")
+	else:
+		print(0, end="")
