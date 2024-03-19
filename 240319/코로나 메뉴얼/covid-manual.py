7022509aa83c32cbc5e90f2d64fa84ca08a11414
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-03-19 22:40:03 +0900

    Title: 코로나 메뉴얼, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240319/코로나 메뉴얼/covid-manual.py
+++ 240319/코로나 메뉴얼/covid-manual.py
@@ -1,24 +1,26 @@
+def check(x,y,a):
+    if x=="Y" and y>=37:
+        a+=1
+    return a
+
+
 A=input().split()
 a1=A[0]
 a2=int(A[1])
 
 B=input().split()
-b1=A[0]
-b2=int(A[1])
+b1=B[0]
+b2=int(B[1])
 
 C=input().split()
-c1=A[0]
-c2=int(A[1])
+c1=C[0]
+c2=int(C[1])
+
 
 a=0
-if a1=="Y" and a2>=37:
-    a+=1
-
-if b1=="Y" and b2>=37:
-    a+=1
-
-if c1=="Y" and c2>=37:
-    a+=1
+a=check(a1,a2,a)
+a=check(b1,b2,a)
+a=check(c1,c2,a)
 
 if a>=2:
     print("E")
