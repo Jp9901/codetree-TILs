a40f8e3c6c6faa7534a4f8052f0b9d062859efd1
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-03-19 21:43:16 +0900

    Title: 4번의 크기 비교, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240319/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,14 @@
+a=int(input())
+y=input()
+b=int(y.split()[0])
+c=int(y.split()[1])
+d=int(y.split()[2])
+e=int(y.split()[3])
+
+print(int(a>b))
+
+print(int(a>c))
+
+print(int(a>d))
+
+print(int(a>e))
