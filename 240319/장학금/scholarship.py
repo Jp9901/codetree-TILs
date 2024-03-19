95fd63fa3b0b663255ecd242c20d119b707fb92a
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-03-19 22:02:26 +0900

    Title: 장학금, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/장학금/scholarship.py
+++ 240319/장학금/scholarship.py
@@ -6,9 +6,9 @@ fin=int(s[1])
 
 if mid<90:
     print(0)
-elif mid>=90 & fin<90:
+elif mid>=90 & (fin<90):
     print(0)
-elif mid>=90 & fin<95:
+elif mid>=90 & (90<=fin & fin<95):
     print(5)
 else:
     print(10)
