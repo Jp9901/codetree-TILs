fec341da39986ad4cf97fbc247773e342475a181
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-05 00:24:39 +0900

    Title: 줄 세우기 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240405/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,10 @@
+n=int(input())
+students=[]
+for i in range(n):
+    a=list(map(int,input().split()))
+    a.append(i+1)
+    students.append(tuple(a))
+
+students.sort(lambda x:x[0])
+for a,b,c in students:
+    print(a,b,c)
