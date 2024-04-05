b7d33288f0cd7ad4a9c5ef815252aec738a33881
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-05 22:13:39 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240405/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,55 @@
+N,M=tuple(map(int,input().split()))
+
+A=[]
+B=[]
+for _ in range(N):
+    s=input()
+    a=s.split()[0]
+    b=int(s.split()[1])
+    c=tuple([a,b])
+    A.append(c)
+
+for _ in range(M):
+    s=input()
+    a=s.split()[0]
+    b=int(s.split()[1])
+    c=tuple([a,b])
+    B.append(c)
+
+A_move=[]
+B_move=[]
+
+# A리스트에 대해 반복하면서, 각 튜플에서 만약 오른쪽이라면
+# 시간만큰 1씩 더하면서 리스트에 append 
+i=0
+for direct, time in A:
+
+    if direct=='R':
+        for _ in range(time):
+            A_move.append(i)
+            i+=1
+
+    else: 
+        for _ in range(time):
+            A_move.append(i)
+            i-=1
+
+#################################
+i=0
+for direct, time in B:
+
+    if direct=='R':
+        for _ in range(time):
+            B_move.append(i)
+            i+=1
+
+    else: 
+        for _ in range(time):
+            B_move.append(i)
+            i-=1
+
+#################################
+for i in range(min(len(A_move),len(B_move))):
+    if A_move[i+1]==B_move[i+1]:
+        print(i+1)
+        break
