ce8bb02744c5950116c2c16885b72b92773ee01c
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-05 21:28:54 +0900

    Title: 연속되는 수 4, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/연속되는 수 4/continuous-number4.py
+++ 240405/연속되는 수 4/continuous-number4.py
@@ -2,15 +2,15 @@ N=int(input())
 
 check=[ int(input()) for _ in range(N)]
 
-cnt=0
-aws=[]
-for i in range(N):
+cnt=1
+aws=1
+for i in range(1,N):
     
     if check[i]>check[i-1]:
         cnt+=1
         
     else:
-        aws.append(cnt+1)
-        cnt=0
+        aws=max(aws,cnt)
+        cnt=1
 
-print(max(aws))
+print(aws)
