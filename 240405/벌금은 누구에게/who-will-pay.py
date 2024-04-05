3271162c4a7d97840ce310f3f1f6bf49a6ec22c7
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-05 23:01:35 +0900

    Title: 벌금은 누구에게, Time: 164ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240405/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,25 @@
+#벌칙을 하나씩 읽으면서 딕셔러니의 값을 1씩 더해주고
+#값이 K가 되면 반복문을 멈추고 키를 출력
+#벌칙을 모두 읽었는데 값이 K가 없으면 -1을 출력한다
+
+
+N,M,K=tuple(map(int,input().split()))
+
+#딕셔너리 생성
+dic={i:0 for i in range(1,N+1) }
+
+def check(dic):
+    for i in range(1,N+1):
+        if dic[i]==K:
+            return i
+
+
+
+
+
+for _ in range(M):
+    x=int(input())
+    dic[x]+=1
+    if check(dic):
+        print(check(dic))
+        break
