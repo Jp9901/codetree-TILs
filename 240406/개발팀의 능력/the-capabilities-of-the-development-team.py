780f14e576698c27f5e24136f885dd7643cc6089
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-06 22:38:58 +0900

    Title: 개발팀의 능력, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240406/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+import sys
+target=list(map(int, input().split()))
+k=sum(target)
+
+index=target
+all=combinations(index,2) 
+cnt=sys.maxsize
+
+for all in all:                         #처음 2개의 인덱스
+    index2=list(set(index)-set(all))    
+    all2=combinations(index2,2)         #그다음 2개의 인덱스
+       
+    for all2 in all2:
+        all3=list(set(index2)-set(all2))  #나머지 1개의 인덱스
+        L=max(sum(all),sum(all2),sum(all3))
+        l=min(sum(all),sum(all2),sum(all3))
+        if (cnt > (L-l)) and (L!=(k-L-l)and l!=(k-L-l)):
+            cnt=(L-l)
+
+
+
+#print(target)
+print(cnt)
