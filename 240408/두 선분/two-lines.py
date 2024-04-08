663a23d15523c604da573ae287520213cf1f8547
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-08 23:05:35 +0900

    Title: 두 선분, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/두 선분/two-lines.py
+++ 240408/두 선분/two-lines.py
@@ -1,10 +1,9 @@
 x1, x2 ,x3, x4=tuple(map(int,input().split()))
 
 
-
-if x2>=x3:
+if x2>=x3 and x1<= x4:
     print('intersecting')
-elif x4>=x1:
+elif x4>=x1 and x2>=x3:
     print('intersecting')
 else:
     print("nonintersecting")
