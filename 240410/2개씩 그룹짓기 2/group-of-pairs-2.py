9d331fcd0564c4fb8c9252b70c83877a2ade9c91
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-10 17:20:58 +0900

    Title: 2개씩 그룹짓기 2, Time: 113ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240410/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -0,0 +1,13 @@
+#i부터 i+3의 차이를 계속 갱신하자. 
+#i는 0부터 -4까지 탐색 
+
+import sys
+n=int(input())
+numbers=list(map(int,input().split()))
+numbers.sort()
+
+value=sys.maxsize
+for i in range(0,len(numbers)-3):
+    value=min(value,abs(numbers[i]-numbers[i+3]))
+
+print(value)
