52c9ad5833eb7077d7908a88d5116895b33dbd4b
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-11 21:30:59 +0900

    Title: 1차원 윷놀이, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,23 @@
+n,m,k = tuple(map(int,input().split()))  #이동횟수, 목표점, 말의 수
+move=list(map(int,input().split()))      #이동거리
+
+# k 1부터 k까지 for문
+#[1, 1, 2, 2, 3, 3 ]
+#이 리스트에 대한 for문으로 result=[0]*k 의 각 위치 값에 이동거리를 더해줌. 
+
+from itertools import combinations_with_replacement as cwr
+all=cwr([i+1 for i in range(k)],n)
+
+aws=0
+for a in all:
+    result=[0]*k
+
+    for i, coin in enumerate(a):
+            result[coin-1]+=move[i]
+    cnt=0
+    for b in result:
+        if b>=m:
+            cnt+=1
+    aws=max(cnt,aws)
+
+print(aws)
