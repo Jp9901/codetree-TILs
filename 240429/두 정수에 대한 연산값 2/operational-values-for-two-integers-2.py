80680e3b2409734860dfee68ad20ca82d4ccf339
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-04-29 17:34:04 +0900

    Title: 두 정수에 대한 연산값 2, Time: 198ms, Memory: 24MB, Status: Passed - CodeTree

--- 240429/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240429/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -3,9 +3,9 @@ a, b = map(int, input().split())
 # 계산 함수
 def cal(n1,n2):
     if n1 > n2:
-        return max(n1,n2)*2, max(n1,n2)+10
+        return n1*2, n2+10
     else:
-        return min(n1,n2)+10, max(n1,n2)*2
+        return n1+10, n2*2
 
 a, b = cal(a,b)
 print(a,b)
