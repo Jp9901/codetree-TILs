18d56a3267fffbfd86f94c44279155270c5f5caa
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-01 20:33:35 +0900

    Title: 중앙값 계산 2, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/중앙값 계산 2/get-median-2.py
+++ 240501/중앙값 계산 2/get-median-2.py
@@ -1,8 +1,14 @@
 n = int(input())
 
-nums = [0]+list(map(int,input().split()))
+# 입력받은 변수들의 리스트
+nums_input = list(map(int,input().split()))
 
-for i in range(1,n+1):
-    if i%2 == 1:
-        nums.sort()
-        print(nums[i//2+1], end=' ')
+# 위 리스트에서 하나씩 추가
+nums_li = list()
+for num in nums_input:
+    nums_li.append(num)
+    lo = len(nums_li)
+
+    if lo%2 == 1:
+        nums_li.sort()
+        print(nums_li[lo//2], end=' ')
