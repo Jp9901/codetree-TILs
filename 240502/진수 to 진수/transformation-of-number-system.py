806575c716bdc4a15685d178a400bce9cdc50c39
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-02 15:03:08 +0900

    Title: 진수 to 진수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/진수 to 진수/transformation-of-number-system.py
+++ 240502/진수 to 진수/transformation-of-number-system.py
@@ -1,13 +1,13 @@
 a, b = map(int,input().split())
 n = list(input())
 
-# 10진수
+# a진수 -> 10진수
 digit_10 = 0
 
 for i in range(len(n)):
-    digit_10  += int(n[-i])*(a**i)
+    digit_10  += int(n[-(i+1)])*(a**i)
 
-# b진수
+# 10진수 -> b진수
 digit_b = list()
 while True:
     if digit_10 < b:
