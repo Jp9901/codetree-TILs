2c64a5c54bdb110290bcbb6e7762036fa6aeb04e
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-02 14:16:25 +0900

    Title: 그 요일은, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/그 요일은/the-day-of-the-day.py
+++ 240502/그 요일은/the-day-of-the-day.py
@@ -6,10 +6,14 @@ num_of_days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 
 elapsed_day = 0
 
+# m1/d1~m2/d2 기간
 if m1 == m2:
     elapsed_day = d2-d1
 else:
     elapsed_day = (sum(num_of_days[m1:m2])-d1)+d2
 
-
-print((elapsed_day+day.index(d1_day))//7)
+# A요일 횟수
+if day.index(d1_day) <= (elapsed_day%7):
+    print(elapsed_day//7+1)
+else:
+    print(elapsed_day//7)
