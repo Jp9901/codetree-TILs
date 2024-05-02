e0ae546231ed593ade7ab6d68a5a22bbc2bc9305
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-02 14:54:16 +0900

    Title: 진수 to 진수, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,20 @@
+a, b = map(int,input().split())
+n = list(input())
+
+# 10진수
+digit_10 = 0
+
+for i in range(len(n)):
+    digit_10  += int(n[-i])*(8**i)
+
+digit_b = list()
+while True:
+    if digit_10 < b:
+        digit_b.append(digit_10)
+        break
+
+    digit_b.append(digit_10 % b)
+    digit_10 //= b
+
+for _ in digit_b[::-1]:
+    print(_, end="")
