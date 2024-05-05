64a4dedaa291c1e011fd2b9a1b97911cb0f222d7
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-05 19:54:52 +0900

    Title: 연속되는 수 4, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240505/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+li = list(int(input()) for _ in range(n))
+
+# 각 횟수와 최대 횟수
+cnt = 1
+max_cnt = 1
+for i in range(1,n):
+    if li[i] > li[i-1]:
+        cnt += 1
+        if cnt > max_cnt:
+            max_cnt = cnt
+
+    else: 
+        cnt = 1
+
+print(max_cnt)
