05168ef9dc3c578c835e102d73ffd36253ab1083
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-05 19:48:01 +0900

    Title: 연속되는 수 2, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240505/연속되는 수 2/continuous-number2.py
+++ 240505/연속되는 수 2/continuous-number2.py
@@ -2,18 +2,20 @@ n = int(input())
 
 li = list(int(input()) for _ in range(n))
 
-
-cnt=1
+# 각 동일 수 횟수와 최댓값
+cnt = 1
 max_cnt = cnt
 for i in range(1,n):
+    
+    # 이전 숫자와 현재 숫자 비교
     if li[i-1] == li[i]:
         cnt += 1
+
+        # 최댓값 갱신
         if cnt > max_cnt:
-            max_cnt=cnt
+            max_cnt = cnt
+
     else:
-        cnt =1
-
-
-
+        cnt = 1
 
 print(max_cnt)
