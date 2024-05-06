74e45009943c7db2079df8d835a388929ed03f3a
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-06 15:00:48 +0900

    Title: 체크판위에서 2, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240506/체크판위에서 2/on-the-checkboard-2.py
+++ 240506/체크판위에서 2/on-the-checkboard-2.py
@@ -11,7 +11,7 @@ start = li[r0][c0]
 # 경우 수
 cnt = 0
 
-# 조건2 : 오른쪽과 아래쪽으로 한 칸 이상 점프 => 첫 행, 첫 열, 마지막 행, 마지막 열 제외
+# 조건2 : 오른쪽과 아래쪽으로 한 칸 이상 점프 => 첫 + 마지막 행열 제외
 # 조건3 : 시작과 도착 점 외에 두 번만 이동
 # 첫번째 이동
 for i in range(r0,r-1):
@@ -20,10 +20,16 @@ for i in range(r0,r-1):
         # 조건1 : 다른 색으로 이동
         if start != li[i][j]:
             position = li[i][j] 
+
             # 두번째 이동
             for i_2 in range(i+1, r-1):
-                for c_2 in range(j+1, c-1):
-                    if position != li[i_2][c_2]:
-                        cnt += 1
+                for j_2 in range(j+1, c-1):
+                    if position != li[i_2][j_2]:
+                        position_2 = li[i_2][j_2] 
+
+                        # 세번째 이동(도착점으로)
+                        if position_2 != li[r-1][c-1]:
+                            # print(i,j,i_2,j_2)
+                            cnt += 1
                 
 print(cnt)
