20a43142830e724d1187f0bc929d7545db2aaf73
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-06 17:14:08 +0900

    Title: 바구니 안의 사탕 2, Time: 127ms, Memory: 28MB, Status: Passed - CodeTree

--- 240506/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240506/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -11,13 +11,14 @@ for _ in range(n):
 # [c-K,c+K]구간에 사탕의 수
 max_cnt = 0
 for c in range(0,101):
-    lower = c-k
-    upper = c+k
+    # 구간의 한계 지정
+    lower = max(c-k,0)
+    upper = min(c+k,100)
+    # if lower <0:
+    #     lower =0
 
-    if lower <0:
-        lower =0
-    if upper > 100:
-        upper = 100
+    # if upper > 100:
+    #     upper = 100
     
     cnt = sum(value[lower:(upper+1)])
     max_cnt = max(max_cnt,cnt)
