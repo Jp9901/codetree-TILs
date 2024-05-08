5d853dc5d8550442aa60a00dbbbb1741a7fe18d8
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-09 00:38:36 +0900

    Title: 두 선분, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/두 선분/two-lines.py
@@ -0,0 +1,17 @@
+x1,x2,x3,x4 = map(int,input().split())
+
+li = [0] * 101
+
+li[x1:(x2+1)] = [1]*(x2-x1)
+
+inter = False
+
+for i in range(x3,x4+1):
+    if li[i] == 1:
+        inter = True
+        break
+
+if inter:
+    print('intersecting')
+else:
+    print('nonintersecting')
