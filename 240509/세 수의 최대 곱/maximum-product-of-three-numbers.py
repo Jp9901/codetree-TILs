c2612c280fa3dc53cb4be754bc0a10e4c15c4228
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-09 12:13:19 +0900

    Title: 세 수의 최대 곱, Time: 5008ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240509/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240509/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,11 +1,14 @@
+import sys
+
 n = int(input())
 num = list(map(int,input().split()))
 
-max_val = 0
+max_val = -sys.maxsize
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
             val = num[i]*num[j]*num[k]
             max_val = max(max_val,val)
+
 
 print(max_val)
