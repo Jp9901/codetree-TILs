fe59280f13fec03b80e9b80c2662b76d3c44377e
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-09 12:57:06 +0900

    Title: 세 수의 최대 곱, Time: 170ms, Memory: 31MB, Status: Passed - CodeTree

--- 240509/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240509/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,14 +1,31 @@
-import sys
-
 n = int(input())
 num = list(map(int,input().split()))
+num.sort(reverse=True)
 
-max_val = -sys.maxsize
-for i in range(n):
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            val = num[i]*num[j]*num[k]
-            max_val = max(max_val,val)
+a,b,c = num[0:3]
+x,y,z = num[-3:]
+
+# 최댓값이 양수일 때,
+if a*b*c > 0 or a*y*z > 0:
+    val = max(a*b*c,a*y*z)
+
+# 최대값이 음수일 때,
+elif a*b*c <0 or a*y*z < 0:
+    val = max(a*b*c,a*y*z)
+
+# 최댓값이 0일 때,
+else:
+    val = 0
 
 
-print(max_val)
+print(val)
+
+
+
+# # 양수 3개
+# if num[0]>0 and num[1]>0 and num[2]>0:
+#     val1 = num[0]*num[1]*num[2]
+# # 양수 1개 + 음수 2개
+# if num[0]>0 and num[-1]<0 and num[2]<0:
+#     val2 = num[0]*num[1]*num[2]
+# max_val = max(val1,val2)
