9dd337e3c75ad2cbf7d40356eff723cf0de18c46
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-11 18:42:51 +0900

    Title: 선택 정렬 구현, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240511/선택 정렬 구현/implement-selection-sort.py
+++ 240511/선택 정렬 구현/implement-selection-sort.py
@@ -1,20 +1,18 @@
-# 선택 정렬 함수
-def sel_sort(front,rear):
-    if front > rear:
-        return rear, front
-    else:
-        return front,rear
-
 n = int(input())
 arr = list(map(int,input().split()))
 
 for i in range(n-1):
-    val = arr[i]
 
-    # i번째 이후의 최솟값
-    rear_val = min(arr[i+1:])
-    rear_idx = arr.index(rear_val)
+    # i번째 값 이후의 최솟값
+    new_arr = arr[i+1:]
+    min_val = min(new_arr)
+    min_idx = (i+1)+new_arr.index(min_val)
 
-    arr[i], arr[rear_idx] = sel_sort(arr[i],arr[rear_idx])
+    # i번째 값이 이후의 최솟값보다 크면,
+    if arr[i] > min_val:
+        # 교체
+        arr[i],arr[min_idx] = arr[min_idx],arr[i]
+    else:
+        continue
 
 print(*arr)
