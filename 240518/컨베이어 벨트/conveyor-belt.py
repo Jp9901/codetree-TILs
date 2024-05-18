501e08796e53dbe33864c6bacdb3d6064d87d12e
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-18 16:57:20 +0900

    Title: 컨베이어 벨트, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240518/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,17 @@
+n, t = tuple(map(int,input().split()))
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+
+arr = arr1
+arr.extend(arr2)
+
+for _ in range(t):
+    tmp = arr[2*n-1]
+    for i in range(2*n-1,0,-1):
+        arr[i] = arr[i-1]
+    arr[0] = tmp
+
+arr1 = arr[0:3]
+arr2 = arr[3:]
+print(*arr1)
+print(*arr2)
