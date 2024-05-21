9ab3e449f31f77006ca2b67df207a7d6c73c4395
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-21 18:12:09 +0900

    Title: 계단 오르기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240521/계단 오르기/climbing-stairs.py
@@ -0,0 +1,23 @@
+n=int(input())
+
+
+dp=[-1]*(n+10)
+
+dp[1]=0
+dp[2]=1
+dp[3]=1
+
+#dp[n]=dp[n-2]+1 or dp[n-3]+1 if 0이 아니라면 
+
+
+for i in range(4,n+1):
+    if dp[n-2]!=0:
+        dp[n]=dp[n-2]+1  
+
+    if dp[n]==-1 and dp[n-3]!=0:
+        dp[n]=dp[n-3]+1 
+
+    if dp[n]==-1:
+        dp[n]=0
+
+print(dp[n])
