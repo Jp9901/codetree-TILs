2d48e7c6d3436ec3c4bbeafadc0937ddfcb1112e
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-22 21:38:51 +0900

    Title: 동전 거슬러주기, Time: 243ms, Memory: 25MB, Status: Passed - CodeTree

--- 240522/동전 거슬러주기/coin-change.py
+++ 240522/동전 거슬러주기/coin-change.py
@@ -3,7 +3,7 @@ import sys
 n,m = map(int,input().split())
 coins = list(map(int,input().split()))
 
-dp =[sys.maxsize]*(m+1)
+dp =[10001]*(m+1)
 
 dp[0] = 0
 for i in range(1,m+1):
@@ -14,4 +14,8 @@ for i in range(1,m+1):
             
             dp[i] = min(dp[i], dp[i-coins[j]]+1)
 
-print(dp[m])
+
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
