51b7e1a8f7e4227902c365c6ce79e09bade88527
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-05-22 17:21:38 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,27 @@
+n,m=map(int,input().split())
+graph=[list(map(int,input().split()))  for _ in range(n)]
+visit=[[False]*m for _ in range(n)]
+visit[0][0]=True
+
+dxs,dys=[0,-1],[1,0]
+
+def can_go(x,y,graph):
+    if 0<=x<n and 0<=y<m and graph[x][y]==1:
+        return True
+    else: 
+        return False
+
+
+def dfs(x,y,graph):
+
+    if x==n-1 and y==m-1:
+        print(1)
+
+    for dx,dy in zip(dxs,dys):
+        nx,ny=x+dx,y+dy
+        if can_go(nx,ny,graph):
+            visit[nx][ny]=True
+            dfs(nx,ny,graph)
+    print(0)
+
+dfs(0,0,graph)
