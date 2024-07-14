6b95c0ad284ad90ded61e25e050d20c68a1ca1ce
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-07-14 16:28:38 +0900

    Title: 연결된 정점, Time: 604ms, Memory: 31MB, Status: Passed - CodeTree

--- 240714/연결된 정점/connected-vertex.py
+++ 240714/연결된 정점/connected-vertex.py
@@ -1,6 +1,7 @@
 n,m=tuple(map(int,input().split()))
 
 club=[1]*(n+1)
+rank = [0] * (n + 1)  # 각 노드의 랭크를 저장
 
 
 uf=[0]*(n+1)
@@ -13,12 +14,26 @@ def find(x):
     uf[x]=find(uf[x])
     return uf[x]
 
-def union(x,y):
-    X,Y=find(x),find(y)
-    uf[X]=Y 
-    club[Y]+=club[X]
-    #club[X]|=club[Y]
-
+# def union(x,y):
+#     X,Y=find(x),find(y)
+#     uf[X]=Y 
+#     club[Y]+=club[X]
+    
+def union(x, y):
+    rootX = find(x)
+    rootY = find(y)
+    
+    if rootX != rootY:
+        if rank[rootX] > rank[rootY]:
+            uf[rootY] = rootX
+            club[rootX] += club[rootY]
+        elif rank[rootX] < rank[rootY]:
+            uf[rootX] = rootY
+            club[rootY] += club[rootX]
+        else:
+            uf[rootY] = rootX
+            club[rootX] += club[rootY]
+            rank[rootX] += 1  # 두 트리의 랭크가 같을 때 하나의 랭크를 증가시킵니다.
 
 def y(k):                #k와 연결되어 있는 노드의 개수 출력 
     mom=find(k)
