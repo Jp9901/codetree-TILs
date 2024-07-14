8f6daa1be10e96e639e8c8030f7630f446b04b51
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-07-14 16:21:38 +0900

    Title: 연결된 정점, Time: 1155ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240714/연결된 정점/connected-vertex.py
+++ 240714/연결된 정점/connected-vertex.py
@@ -1,5 +1,11 @@
 n,m=tuple(map(int,input().split()))
 
+club=[]
+for i in range(n+1):
+    my_set={i}
+    club.append(my_set)
+
+
 uf=[0]*(n+1)
 for i in range(1,n+1):
     uf[i]=i
@@ -13,26 +19,20 @@ def find(x):
 def union(x,y):
     X,Y=find(x),find(y)
     uf[X]=Y 
+    club[Y]|=club[X]
+    #club[X]|=club[Y]
 
 
-def x(a,b):
-    union(a,b)
-
-def y(k):
+def y(k):                #k와 연결되어 있는 노드의 개수 출력 
     mom=find(k)
-    ans=0
-    for i in range(1,n+1):
-        if find(i)==mom:
-            ans+=1
-    print(ans)
+    print(len(club[mom]))
 
-        
 
 for _ in range(m):
     command=input().split()
     if command[0]=="x":
         a,b=int(command[1]),int(command[2])
-        x(a,b)
+        union(a,b)
     else: 
         k=int(command[1])
         y(k)
