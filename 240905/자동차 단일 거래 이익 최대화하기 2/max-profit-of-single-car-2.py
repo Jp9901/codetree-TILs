8f347312ca7571431b5e9f4074eb8c27f6c9bca3
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-09-05 21:16:44 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240905/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240905/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -6,14 +6,14 @@ sp=sorted(price, reverse=True)
 ans=0
 i=0
 
-if sp==price:
-    print(0)
+# if sp==price:
+#     print(0)
 
-else:
-    while price:   
+# else:
+while price:   
         i=price.index(max(price))
         small_p=price[:i]
         ans=max(max(price)-min(small_p),ans)
         price=price[i+1:]
     
-    print(ans)
+print(ans)
