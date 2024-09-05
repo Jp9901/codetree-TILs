6b416eff1b02f3d62e8d769e2c2f5c2919e93da9
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-09-05 20:54:13 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 157ms, Memory: 31MB, Status: Passed - CodeTree

--- 240905/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240905/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -24,6 +24,24 @@
 #         price=price[i+1:]
     
 #     print(ans)
+########################################################
+# n = int(input())
+# prices = list(map(int, input().split()))
+
+# # 최소값과 최대 차이 초기화
+# min_price = float('inf')
+# max_diff = 0
+
+# # 리스트를 한 번만 순회하면서 계산
+# for price in prices:
+#     if price < min_price:
+#         min_price = price  # 최소값 갱신
+#     current_diff = price - min_price  # 현재 값과 최소값의 차이 계산
+#     if current_diff > max_diff:
+#         max_diff = current_diff  # 최대 차이 갱신
+
+# print(max_diff)
+########################################################
 
 n = int(input())
 prices = list(map(int, input().split()))
@@ -37,7 +55,6 @@ for price in prices:
     if price < min_price:
         min_price = price  # 최소값 갱신
     current_diff = price - min_price  # 현재 값과 최소값의 차이 계산
-    if current_diff > max_diff:
-        max_diff = current_diff  # 최대 차이 갱신
+    max_diff=max(max_diff,current_diff)
 
 print(max_diff)
