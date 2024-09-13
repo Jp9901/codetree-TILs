1a2d1e23f6fc1e73f156b3830a879e6bb2544e17
junpyo.lee <135187850+Jp9901@users.noreply.github.com>
2024-09-13 15:58:13 +0900

    Title: 트리 판별, Time: 161ms, Memory: 26MB, Status: Passed - CodeTree

--- 240913/트리 판별/tree-identification.py
+++ 240913/트리 판별/tree-identification.py
@@ -1,49 +1,50 @@
-m = int(input())
+from collections import deque, defaultdict
+import sys
 
-# 그래프 초기화 및 노드 정보
-graph = {}
-indegree = {}
+input = sys.stdin.read
+data = input().split()
+
+m = int(data[0])
+graph = defaultdict(list)
+indegree = defaultdict(int)
+
+if m == 0:
+    print(1)
+    sys.exit()
+
 nodes = set()
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    if a not in graph:
-        graph[a] = []
-    if a not in indegree:
-        indegree[a] = 0
-    if b not in indegree:
-        indegree[b] = 0
+for i in range(1, len(data), 2):
+    a = int(data[i])
+    b = int(data[i+1])
     graph[a].append(b)
     indegree[b] += 1
     nodes.update([a, b])
 
-# 루트 노드 찾기
 root = None
 for node in nodes:
     if indegree[node] == 0:
-        if root is not None:  # 두 번째 루트 발견 시 바로 0 출력
+        if root is not None:
             print(0)
-            exit()
+            sys.exit()
         root = node
 
-if root is None:  # 루트 노드가 없는 경우도 트리가 아님
+if root is None:
     print(0)
-    exit()
+    sys.exit()
 
-# BFS 실행하여 모든 노드 방문 확인
 visit = set()
-queue = [root]
+q = deque([root])
 
-while queue:
-    current = queue.pop(0)
-    if current in visit:
-        continue
-    visit.add(current)
-    if current in graph:
-        for neighbor in graph[current]:
-            queue.append(neighbor)
+while q:
+    node = q.popleft()
+    if node in visit:
+        print(0)
+        sys.exit()
+    visit.add(node)
+    for n in graph[node]:
+        q.append(n)
 
-# 방문한 노드 수와 총 노드 수 비교
 if len(visit) == len(nodes):
     print(1)
 else:
